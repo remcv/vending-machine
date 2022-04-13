41456b5ddc5219aead8eec98670b0ffd8bb1871f
remcv <remuluscatana@tutanota.com>
2022-04-13 06:53:24 +0300

    Initial Maven project setup (to be updated)

---
+++ src/main/java/remcv/com/github/vendingmachine/App.java
@@ -0,0 +1,13 @@
+package remcv.com.github.vendingmachine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
